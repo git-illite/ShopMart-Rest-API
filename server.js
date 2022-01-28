19ce4f22db1d5ea3b1285b227dca371cefd8bd76
Abdallah Aden <abdallah.aden@gmail.com>
2022-01-27 20:51:24 -0500

    installed dotenv and made keys file

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose")
+require('dotenv').config({ path: 'config/keys.env' });
 
 const app = express();
 
@@ -6,4 +8,13 @@ const PORT = 3000;
 
 app.listen(PORT,()=>{
     console.log(`ShopMart Api listening on ${PORT}`);
+
+    mongoose.connect(process.env.MONGODB_QUERY_STRING)
+    .then(()=>{
+        console.log(`Connected to MongoDb ShopMart`)
+    })
+    .catch(err=>{
+        console.log(`Error ${err}`)
+    })
+
 })
